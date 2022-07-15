8e1f8099782011461a2cc0e87ba00610f936104c
Madison Cairns <mcairns1@depaul.edu>
2022-07-14 22:05:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { settings } from "./configs/game";
+import stone from "./assets/stone.png";
+import paper from "./assets/paper.png";
+import scissors from "./assets/scissors.png";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Rock Paper Scissors Game</p>
     </div>
   );
 }
-
-export default App;
